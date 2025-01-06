48af8c0cb092e78811f8319eab46b0093c78c818
Muhammad Sami <muhammadsami290@outlook.com>
2025-01-06 23:23:06 +0500

    Fix code for finding jar file

--- scripts/run.py
+++ scripts/run.py
@@ -6,14 +6,6 @@ SPARK_PIPELINE = PROJECT_ROOT.joinpath('sparkpipeline')
 TARGET_FOLDER = PROJECT_ROOT.joinpath(SPARK_PIPELINE).joinpath('target')
 
 
-def find_jar_file() -> str:
-    for file in TARGET_FOLDER.iterdir():
-        if (str(file).endswith('.jar')):
-            return file
-    raise FileNotFoundError(
-        f'No jar file found in {TARGET_FOLDER} folder. Run mvn commands to first build the jar file.')
-
-
 def run_maven() -> None:
     """Executes the maven commands validate, install and package command."""
     command = 'mvn validate && mvn clean && mvn install && mvn package'
@@ -22,6 +14,14 @@ def run_maven() -> None:
         subprocess.run(command, cwd=SPARK_PIPELINE, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f'mvn command failed: {e}')
+
+
+def find_jar_file() -> str:
+    for file in TARGET_FOLDER.iterdir():
+        if (str(file).endswith('.jar')):
+            return file
+    raise FileNotFoundError(
+        f'No jar file found in {TARGET_FOLDER} folder. Run mvn commands to first build the jar file.')
 
 
 def run_spark_submit(jar_file: str) -> None:
@@ -36,8 +36,8 @@ def run_spark_submit(jar_file: str) -> None:
 
 if __name__ == '__main__':
     try:
-        jar_file = find_jar_file()
         run_maven()
+        jar_file = find_jar_file()
         run_spark_submit(jar_file)
     except Exception as e:
         print(f'Pipeline execution failed! {e}')
